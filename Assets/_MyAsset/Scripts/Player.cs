49408572764723602fa6e58c575f977f50a12327
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2023-05-02 13:55:27 -0400

    Debut liaison animation
    Animation idle to walk
    Reste en walk

--- Assets/_MyAsset/Scripts/Player.cs
+++ Assets/_MyAsset/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -17,6 +15,53 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Mouvement();
+    }
+
+    void Mouvement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(x, y, 0f);
+
+        transform.Translate(direction * Time.deltaTime * vitesse);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 2), 0f);
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+        {
+            playerAnim.SetBool("Mouvements", true);
+
+
+
+        }
+ 
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                playerAnim.SetBool("Cour", true);
+            }
+            else
+            {
+                playerAnim.SetBool("Cour", !true);
+            }
         
+        //if (x < 0f)
+        //{
+        //    playerAnim.SetBool("Droite", true);
+        //    playerAnim.SetBool("Gauche", !true);
+        //}
+        //else if (x > 0f)
+        //{
+        //    playerAnim.SetBool("Droite", !true);
+        //    playerAnim.SetBool("Gauche", true);
+        //}
+        //else
+        //{
+        //    playerAnim.SetBool("Droite", !true);
+        //    playerAnim.SetBool("Gauche", !true);
+        //}
+
+
+
     }
 }
