67f17d26c9bec5a373025aab2feb9bb253ddc598
Jeanmi790 <102696659+Jeanmi790@users.noreply.github.com>
2023-05-20 16:38:16 -0400

    InGameUISetup

--- Assets/_MyAsset/Scripts/Managers/ManagerUI.cs
+++ Assets/_MyAsset/Scripts/Managers/ManagerUI.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ManagerUI : MonoBehaviour
 {
+    [Header("OptionPanel")]
+    [SerializeField] private GameObject OptionPanel = default;
+
+    [Header("PausePanel")]
+    [SerializeField] private GameObject PausePanel = default;
+
+    [Header("GameOverPanel")]
+    [SerializeField] private GameObject GameOverPanel = default;
+
+    [Header("InstructionPanel")]
+    [SerializeField] private GameObject InstructionPanel = default;
+
+    private bool isPaused;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        OptionPanel.SetActive(false);
+        PausePanel.SetActive(false);
+        GameOverPanel.SetActive(false);
+        InstructionPanel.SetActive(false);
+        isPaused = false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PauseGame();
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    private void PauseGame()
+    {
+        if (Input.GetKey(KeyCode.Escape) && !isPaused)
+        {
+            Time.timeScale = 0;
+            OpenPanel(PausePanel);
+            isPaused = true;
+        }
+        else if (Input.GetKey(KeyCode.Escape) && isPaused)
+        {
+            Time.timeScale = 1;
+            ClosePanel(PausePanel);
+            isPaused = !true;
+        }
     }
 }
